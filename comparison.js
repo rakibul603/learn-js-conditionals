caf768044f03414ce254e696f3a28bd9aee1af10
rakib <rakibco66@gmail.com>
2026-01-28 21:01:22 +0600

    try to learn comparition operator

---
+++ comparison.js
@@ -0,0 +1,34 @@
+/**  comparison operator  
+
+1. bigger : >
+2. less : <
+3. equal : ==
+4. greater then or equal : >=
+5. less than or equal : <= 
+6. not equal : != 
+
+
+-------------------
+7. and : && 
+8. or : ||
+
+*/ 
+
+// console.log(5>10)
+// console.log(5 < 10)
+// console.log(5 === 10)
+
+
+let peyaraPrice = 100;
+let applePrice = 250;
+
+
+// console.log(peyaraPrice > applePrice)
+console.log(peyaraPrice < applePrice)
+
+
+console.log("10" == 10)
+console.log("10" === 10)
+
+
+
---
+++ contiaon.js
@@ -0,0 +1,11 @@
+let id = Symbol("id");
+console.log(id)
+
+
+let person = {
+  name: "Rakib",
+  age: 22,
+  isStudent: true
+};
+
+console.log(person.name)
