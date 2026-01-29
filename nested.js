6a69e19d7862a9ff32ddc4aa19541313a139c84a
rakib <rakibco66@gmail.com>
2026-01-29 22:13:05 +0600

    learn nestend if statement

---
+++ nested.js
@@ -0,0 +1,20 @@
+const money = 0;
+
+if (money > 300) {
+    console.log('bro you are rich !!!')
+}
+
+else { 
+    if (money > 100) { 
+        console.log("tui gorib oona brolok o na")
+    }
+
+    else { 
+        if(money > 0) {
+            console.log('bondu twi kola kha')
+        }
+        else {
+            console.log('twi gorib')
+        }
+    }
+}
