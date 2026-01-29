78b863d219795e7c2166ab3612c0cdc62695e02e
rakib <rakibco66@gmail.com>
2026-01-29 21:17:48 +0600

    try to explore if statment

---
+++ if.js
@@ -0,0 +1,25 @@
+// if (13 < 10) {
+//     console.log('I have a smaller value')
+// }
+// else{
+//     console.log('big value')
+// }
+
+
+// let birayniPrice = 500;
+
+// if (birayniPrice > 300) {
+//         console.log('mama give me biriayni')
+// }
+
+
+
+ let weight = 40;
+
+ if (weight >= 40) {
+    console.log("I will carry it by myself")
+ }
+ else{ 
+    console.log('i will  rent a rickshaw')
+ }
+
