76097c504354da8da81ae091ccb6322e17fbc6a3
rakib <rakibco66@gmail.com>
2026-01-29 21:38:42 +0600

    learn multicondition in if statment

---
+++ multipleconditon.js
@@ -0,0 +1,34 @@
+const salary = 25000;
+
+const isBCS = true;
+const height = 68;
+
+const hasCar = false;
+
+// if(salary > 20000) {
+//     console.log('suu---patro')
+// }
+// else {
+//     console.log('onno patro khuji')
+// }
+
+// if (salary > 20000 && height > 66) { 
+//     console.log("su-patro")
+// }
+// else{
+//     console.log('onno patro khuji')
+// }
+
+// if (salary > 25000 || height > 76 || isBCS === true) { 
+//     console.log("su-patro")
+// };
+// else{
+//     console.log('onno patro khuji')
+// };
+
+
+// complex condition 
+
+if ((salary > 25000 && hasCar === true) || isBCS === true) {
+    console.log('tomar 14 gosti raji')
+};
