42497204fbea516d6ef8241ea155e2f0816135d0
rakib <rakibco66@gmail.com>
2026-02-01 17:42:06 +0600

    learn logical not operator

---
+++ shorcurt.js
@@ -0,0 +1,21 @@
+const isLeader = true;
+
+
+if (isFinite) {
+    console.log("leader asche ! chair charo")
+}
+else { 
+    console.log("vitory dukte dibi na")
+}
+
+
+const isPassed = false ;
+// if (isPassed === false)
+if (!isPassed) {
+    console.log("toke biya diya dimu")
+}
+
+else {
+    console.log('tumi amader noyon er moni')
+}
+
--- ternariy.js
+++ ternariy.js
@@ -41,6 +41,10 @@ const isLeader = false;
 // }
 
 
-price = isLeader === true ? price > 1000 ? price = price / 2 : price = 0 : price + 1000;
+price = isLeader === true ? price > 1000 ? price = price / 2 :
+ price = 0 : 
+ price + 1000;
 
 console.log(price)
+
+
